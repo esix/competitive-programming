2c84099ed2b39672a0e43c18056b238fb2a1eb1f
esix <esix@live.ru>
2015-05-31 20:01:23 +0300

    ACCEPTED mccme:468

---
+++ mccme/468/solution.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+n, m = map(int, raw_input().split(' ')[0:2])
+
+edges = set()
+
+for i in xrange(m):
+    a, b = map(int, raw_input().split(' ')[0:2])
+    if (a,b) in edges or (b,a) in edges:
+        print "YES"
+        exit()
+    edges.add((a,b))
+
+print "NO"
