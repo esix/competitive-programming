47170879aa46d0da9f66750101910374f3f0a27c
esix <esix@live.ru>
2015-06-14 14:19:08 +0300

    ACCEPTED acmp:0006

---
+++ acmp/0006/solution.pas
@@ -0,0 +1,40 @@
+var
+  input, output: Text;
+  s: String;
+
+function isValidCol(c:char): boolean;
+begin
+  isValidCol := c in ['A'..'H'];
+end;
+
+function isValidRow(c: char): boolean;
+begin
+  isValidRow := c in ['1'..'8'];
+end;
+
+begin
+  assign(input, 'input.txt');
+  reset(input);
+  assign(output, 'output.txt');
+  rewrite(output);
+
+  readln(input, s);
+
+  if (length(s) = 5) and
+     isValidCol(s[1]) and
+     isValidRow(s[2]) and
+     (s[3] = '-') and
+     isValidCol(s[4]) and
+     isValidRow(s[5]) then
+    begin
+      if abs(ord(s[1]) - ord(s[4])) + abs(ord(s[2])-ord(s[5])) = 3 then
+        writeln(output, 'YES')
+      else
+        writeln(output, 'NO')
+    end
+  else
+    writeln(output, 'ERROR');
+
+  close(input);
+  close(output);
+end.
--- acmp/pascal.md
+++ acmp/pascal.md
@@ -1,5 +1,7 @@
 Free Pascal 2.6.4 solutions:
 
 - [0002](0002/solution.pas) - Сумма
-- [0003][0003/solution.pas) - Пятью пять - двадцать пять!
-- [0004][0004/solution.pas) - Игра
+- [0003](0003/solution.pas) - Пятью пять - двадцать пять!
+- [0004](0004/solution.pas) - Игра
+- [0005](0005/solution.pas) - Статистика
+- [0006](0006/solution.pas) - Шахматы
