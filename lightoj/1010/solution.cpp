dd878c7d4a80b3c0fbe41963ddaceb2f4b7caf3e
esix <esix@live.ru>
2015-06-18 15:57:16 +0300

    ACCEPTED lightoj:1010

--- lightoj/1010/solution.cpp
+++ lightoj/1010/solution.cpp
@@ -1,39 +1,40 @@
-#include <iostream>
-
-using namespace std;
-
+#include <stdio.h>
 
 int main() {
-    ios::sync_with_stdio(false);
     int T;
-    cin >> T;
+    scanf("%d", &T);
     
     for(int t = 1; t <= T; t++) {
         int m, n;
-        cin >> m >> n;
+        scanf("%d %d ", &m, &n);
         
-        int result = (m /2)* (n/2) * 2;
-        bool m_is_odd = (m % 2 == 1);
-        bool n_is_odd = (n % 2 == 1);
+        if(m > n) {
+            int x = m; m = n; n = x;
+        }
+        int result;
+        
+        if(m == 1) {
+            result = n;
 
-        if(m_is_odd) {
-            result += n / 2;
-        }
-        if(n_is_odd) {
-            result += m / 2;
-        }
-        if(m_is_odd && n_is_odd) {
-            result ++;
+        } else if(m == 2) {
+            if     (n % 4 == 0) result = n;
+            else if(n % 4 == 1) result = n + 1;
+            else if(n % 4 == 2) result = n + 2;
+            else if(n % 4 == 3) result = n + 1;
+
+        } else {
+            result = (m/2) * (n/2) * 2;
+            int m_is_odd = (m % 2 == 1);
+            int n_is_odd = (n % 2 == 1);
+
+            if(m_is_odd) result += n / 2;
+            if(n_is_odd) result += m / 2;
+            if(m_is_odd && n_is_odd) result++;
+
         }
         
-        if(m == 1) result = n;
-        else if(n == 1) result = m;
-        else if(m == 2 && n == 2) result = 4;
-        else if(m == 2 && n == 3) result = 4;
-        else if(m == 3 && n == 2) result = 4;
-        
-        
-        cout << "Case " << t << ": " << result << endl;
+        //cout << "Case " << t << ": " << result << "\n";
+        printf("Case %d: %d\n", t, result);
     }
 
     return 0;
