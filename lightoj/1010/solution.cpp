385673ea3627f9f435876a35b2fd8c4329b95ff6
esix <esix@live.ru>
2015-06-18 14:21:59 +0300

    ACCEPTED lightoj1015

--- lightoj/1010/solution.cpp
+++ lightoj/1010/solution.cpp
@@ -12,7 +12,25 @@ int main() {
         int m, n;
         cin >> m >> n;
         
-        long long int result;
+        int result = (m /2)* (n/2) * 2;
+        bool m_is_odd = (m % 2 == 1);
+        bool n_is_odd = (n % 2 == 1);
+
+        if(m_is_odd) {
+            result += n / 2;
+        }
+        if(n_is_odd) {
+            result += m / 2;
+        }
+        if(m_is_odd && n_is_odd) {
+            result ++;
+        }
+        
+        if(m == 1) result = n;
+        else if(n == 1) result = m;
+        else if(m == 2 && n == 2) result = 4;
+        else if(m == 2 && n == 3) result = 4;
+        else if(m == 3 && n == 2) result = 4;
         
         
         cout << "Case " << t << ": " << result << endl;
--- lightoj/c++.md
+++ lightoj/c++.md
@@ -4,6 +4,8 @@ C++ solutions:
 - [1002](1002/solution.cpp) - Country Roads
 - [1006](1006/solution.cpp) - Hex-a-bonacci
 - [1008](1008/solution.spp) - Fibsieve`s Fantabulous Birthday
+- [1010](1010/solution.cpp) - Knights in Chessboard
+- [1015](1015/solution.cpp) - Brush (I)
 - [1189](1189/solution.cpp) - Sum of Factorials
 - [1238](1238/solution.cpp) - Power Puff Girls
 - [722:C](_contest-722/c.cpp) - Lollipop
