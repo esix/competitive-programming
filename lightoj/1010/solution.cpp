3ea6345513d6193f70948b33b75e5688b5d02ff7
esix <esix@live.ru>
2015-06-18 12:03:52 +0300

    working

---
+++ lightoj/1010/solution.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    int T;
+    cin >> T;
+    
+    for(int t = 1; t <= T; t++) {
+        int m, n;
+        cin >> m >> n;
+        
+        long long int result;
+        
+        
+        cout << "Case " << t << ": " << result << endl;
+    }
+
+    return 0;
+}
