fa880e885fed299c28a54c9b7477ae2898d14334
esix <esix@live.ru>
2015-07-04 00:55:24 +0300

    shorter acmp:0040

--- acmp/0040/solution.py
+++ acmp/0040/solution.py
@@ -1,12 +1,1 @@
-#!/usr/bin/env python3
-
-inp = open('input.txt', 'r')
-out = open('output.txt', 'w')
-
-
-n = int(inp.readline())
-answer = str(2**n)
-
-
-print(answer)
-out.write(answer)
+open('output.txt', 'w').write(str(2**int(open('input.txt').readline())))
