d3602df9a5f5177079ee2ea8c4658246a3d222aa
esix <esix@live.ru>
2015-07-19 22:16:00 +0300

    ACCEPTED timus:1313

---
+++ timus/1313/README.md
@@ -0,0 +1,1 @@
+# [К вопросу о спорте](http://acm.timus.ru/problem.aspx?space=1&num=1313)
---
+++ timus/1313/solution.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+  fld := make([][]int, n)
+  for i := 0; i < n; i++ {
+    fld[i] = make([]int, n)
+    for j := 0; j < n; j++  {
+      fmt.Scan(&fld[i][j])
+    }
+  }
+  r := 0
+  c := 0
+  for  c < n && r < n {
+    fmt.Print(fld[r][c], " ")
+    r-=1
+    c+=1
+    if c == n  {
+      c = r + 2
+      r = n - 1
+    } else if r == -1 {
+      r = c
+      c = 0
+    }
+  }
+}
