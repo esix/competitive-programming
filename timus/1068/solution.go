106e9870352125981197d96050c6514b8723d530
esix <esix@live.ru>
2015-07-20 22:39:59 +0300

    ACCEPTED timus:1068

---
+++ timus/1068/solution.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func abs(x int) int {
+  if x < 0 {
+    return -x
+  } else {
+    return x
+  }
+
+}
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+  fmt.Print((1 + n)*(1+abs(n-1))/2)
+}
