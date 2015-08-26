92e65f1c2d7a5b87babc2ae022f1c15bb82e77fe
esix <esix@live.ru>
2015-08-26 13:46:19 +0300

    optimizations

--- acmp/0001/solution.py
+++ acmp/0001/solution.py
@@ -1,12 +1,1 @@
-#!/usr/bin/env python3
-
-inp = open('input.txt', 'r')
-out = open('output.txt', 'w')
-
-
-a, b = map(int,inp.readline().split(' ')[0:2])
-answer = str(a+b)
-
-
-print(answer)
-out.write(answer)
+print(sum(map(int,input().split(' '))))
--- acmp/0011/solution.py
+++ acmp/0011/solution.py
@@ -1,17 +1,10 @@
-inp = open('input.txt', 'r')
-out = open('output.txt', 'w')
-
-k, n = map(int, inp.readline().split(' '))
-
-
+k, n = map(int, input().split(' '))
 cache = [0]
-
 for i in range(1, n+1):
     result = 0
     if i <= k:
         result = 1
     result += sum(cache[max(i-k, 1): i])
     cache.append(result)
-    #print " Step ", i , " : " , result, " slice", cache[max(i-k, 1): i]
-    
-out.write(str(cache[n]))
+
+print(cache[n])
