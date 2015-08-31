4dda1541d9673a5df06c99d1b0dafd369212cc4a
esix <esix@live.ru>
2015-08-31 23:21:16 +0300

    ACCEPTED acmp:0102

--- acmp/0102/solution.py
+++ acmp/0102/solution.py
@@ -1,9 +1,4 @@
-A = tuple(map(int, input().split(' ')))
-B = tuple(map(int, input().split(' ')))
-C = tuple(map(int, input().split(' ')))
-O = tuple(map(int, input().split(' ')))
-
+A,B,C,O = [list(map(int, input().split(' '))) for i in range(4)]
 def S(A,B,C):
     return abs((B[0]-A[0])*(C[1]-A[1]) - (B[1]-A[1])*(C[0]-A[0]))
-
-print('In' if S(A,B,C) == S(A,B,O)+S(B,C,O)+S(C,A,O) else 'Out')
+print(['Out','In'][S(A,B,C) == S(A,B,O)+S(B,C,O)+S(C,A,O)])
