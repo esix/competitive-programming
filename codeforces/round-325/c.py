ad232a5579701098a8ca1e0fc98a9414719a7a13
esix <esix@live.ru>
2015-10-13 16:54:13 +0300

    Solved 585A

--- codeforces/round-325/c.py
+++ codeforces/round-325/c.py
@@ -14,25 +14,34 @@ for i in range(n):
     else:
         res.append(i+1)
 
+    crazy_indices = []
+
+    delta_p = v[i]
+
     for j in range(i+1, n):
-        if p[j] is not None:
-            delta_p = v[i] - (j - i - 1)
+        if p[j] is None:
+            continue
 
-            if delta_p <= 0:
-                break
+        if p[j] >= delta_p:
+            p[j] -= delta_p
+        else:
+            p[j] = None
+            crazy_indices.append(j)
 
-            if p[j] >= delta_p:
-                p[j] -= delta_p
-            else:
-                p[j] = None
-                delta_p = d[j]
-                for k in range(j+1, n):
-                    if p[k] is not None:
-                        if p[k] >= delta_p:
-                            p[k] -= delta_p
-                        else:
-                            p[k] = None
-                            delta_p += d[k]
+        delta_p -= 1
+        if delta_p <= 0:
+            break
+
+
+    for j in crazy_indices:
+        delta_p = d[j]
+        for k in range(j+1, n):
+            if p[k] is not None:
+                if p[k] >= delta_p:
+                    p[k] -= delta_p
+                else:
+                    p[k] = None
+                    delta_p += d[k]
 
 print(len(res))
 print(*res)
