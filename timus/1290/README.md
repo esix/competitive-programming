921cc2e7e6a7fc1490582802fd5614223c292983
esix <esix@live.ru>
2015-10-24 17:21:19 +0300

    ACCEPTED timus:1209

---
+++ timus/1290/README.md
@@ -0,0 +1,1 @@
+# [Саботаж](http://acm.timus.ru/problem.aspx?space=1&num=1290)
---
+++ timus/1290/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&a[i])
+	}
+	sort.Ints(a)
+	for i := n - 1; i >= 0; i-- {
+		fmt.Println(a[i], " ")
+	}
+}
