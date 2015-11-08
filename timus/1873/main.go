8ab2aba2722f92a173e8c5efe4f10e2eda97fd70
esix <esix@live.ru>
2015-11-08 15:53:46 +0300

    ACCEPTED timus:1873

---
+++ timus/1873/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+	switch n {
+	case 0:
+		fmt.Println("5")
+	case 1:
+		fmt.Println("20")
+	case 2:
+		fmt.Println("12")
+	case 3:
+		fmt.Println("2")
+	case 4:
+		fmt.Println("1")
+	case 5:
+		fmt.Println("4")
+	case 6:
+		fmt.Println("6")
+	case 7:
+		fmt.Println("1")
+	case 8:
+		fmt.Println("4")
+	case 9:
+		fmt.Println("4")
+	case 10:
+		fmt.Println("1")
+	case 11:
+		fmt.Println("0")
+	}
+}
