daf71eab9e2033a5cbe1f74de98a65759e4ad14c
esix <esix@live.ru>
2015-11-25 16:04:38 +0300

    big comparision

--- _snippets/big.h
+++ _snippets/big.h
@@ -12,62 +12,103 @@ public:
   DecInt();
   DecInt(const std::string&);
   DecInt(int v);
+  DecInt(const DecInt& other);
 
   static DecInt ZERO;
   static DecInt ONE;
 
-  unsigned int digits_count() const;
-  bool         is_zero() const;
-  std::string  to_str() const;
-  operator     std::string() const { return to_str(); }
+  unsigned int    digits_count()  const   { return _ds.size();                 }
+  bool            is_zero()       const   { return digits_count() == 0;        }
+  bool            is_negative()   const   { return _is_negative && !is_zero(); }
+  bool            is_positive()   const   { return !_is_negative && !is_zero();}
+  std::string     to_str()        const;
+  operator        std::string()   const   { return to_str();                   }
+  operator        bool()          const   { return !is_zero();                 }
+
+  DecInt& operator=(const DecInt& other);
+  friend bool operator==(const DecInt& a, const DecInt& b);
+  friend bool operator<(const DecInt& a, const DecInt& b);
 
 
 private:
-  std::deque<char> ds; //digits
-  bool is_negative;
+  std::deque<char>    _ds;           //digits
+  bool                _is_negative;
 };
 
 DecInt DecInt::ZERO = DecInt(0);
 DecInt DecInt::ONE = DecInt(1);
 
 char _to_letter(char c) { return c + '0'; }
+
 char _from_letter(char c) {
   if(!('0' <= c && c <= '9')) throw std::invalid_argument("unknown character");
   return c - '0';
 }
 
-DecInt::DecInt(): is_negative(false) {}
-DecInt::DecInt(const std::string& v): is_negative(false) {
+DecInt::DecInt() : _is_negative(false) {}
+
+DecInt::DecInt(const std::string& v) : _is_negative(false) {
   std::string::const_iterator i = v.begin();
   if(i == v.end()) { throw std::invalid_argument("big::DecInt: zero size"); }
 
-  if(*i == '-')  { is_negative = true; ++i; }
-  else if(*i == '+') {is_negative = false; ++i; }
+  if     (*i == '-') { _is_negative = true;  ++i; }
+  else if(*i == '+') { _is_negative = false; ++i; }
   else if(!('0' <= *i && *i <= '9')) throw std::invalid_argument("unknown character");
 
-  for(;i != v.end() && *i == '0'; ++i);
+  for( ; i != v.end() && *i == '0'; ++i);
 
-  ds = std::deque<char>();
-  std::transform(i, v.end(), std::front_inserter(ds), _from_letter);
+  _ds = std::deque<char>();
+  std::transform(i, v.end(), std::front_inserter(_ds), _from_letter);
 }
-DecInt::DecInt(int v): is_negative(false) {
-  if(v < 0) { v = -v; is_negative = true; }
+
+DecInt::DecInt(int v): _is_negative(false) {
+  if(v < 0) { v = -v; _is_negative = true; }
   while(v != 0) {
-    ds.push_back(v%10);
+    _ds.push_back(v%10);
     v /= 10;
   }
 }
-unsigned int DecInt::digits_count() const { return ds.size(); }
-bool DecInt::is_zero() const{ return digits_count() == 0; }
 
-std::string  DecInt::to_str() const {
+DecInt::DecInt(const DecInt& other) : _ds(other._ds), _is_negative(other._is_negative)
+{}
+
+
+std::string DecInt::to_str() const {
   if(is_zero()) return "0";
   std::string result;
-  if(is_negative) {
-    result = "-";
-  }
-  std::transform(ds.rbegin(), ds.rend(), std::back_inserter(result), _to_letter);
+  if(_is_negative) result = "-";
+  std::transform(_ds.rbegin(), _ds.rend(), std::back_inserter(result), _to_letter);
   return result;
 }
 
+DecInt& DecInt::operator=(const DecInt& other) {
+  _ds = other._ds;
+  _is_negative = other._is_negative;
+  return *this;
 }
+
+inline bool operator==(const DecInt& a, const DecInt& b) {
+  return (a._is_negative == b._is_negative) && (a._ds == b._ds);
+}
+inline bool operator<(const DecInt& a, const DecInt& b) {
+  if(a._is_negative != b._is_negative) return a._is_negative;
+  if     (a.digits_count() < b.digits_count())  return !a._is_negative;
+  else if(a.digits_count() > b.digits_count())  return a._is_negative;
+  else return !a._is_negative ? (a._ds < b._ds) : (a._ds > b._ds);
+}
+inline bool operator!=(const DecInt& a, const DecInt& b) {
+  return !(a == b);
+}
+inline bool operator>(const DecInt& a, const DecInt& b) {
+  return b < a;
+}
+inline bool operator<=(const DecInt& a, const DecInt& b) {
+  return !(b < a);
+}
+inline bool operator>=(const DecInt& a, const DecInt& b) {
+  return !(a < b);
+}
+
+
+
+} //namespace big
--- _snippets/t/big-dec-test.cpp
+++ _snippets/t/big-dec-test.cpp
@@ -9,33 +9,159 @@
 
 
 TEST(BigDecInt, TestInit) {
-  ASSERT_EQ(big::DecInt("1").digits_count(), 1);
-  ASSERT_EQ(big::DecInt("-1").digits_count(), 1);
-  ASSERT_EQ(big::DecInt("+1").digits_count(), 1);
-  ASSERT_EQ(big::DecInt("001").digits_count(), 1);
-  ASSERT_EQ(big::DecInt("-001").digits_count(), 1);
-  ASSERT_EQ(big::DecInt("+001").digits_count(), 1);
-  ASSERT_EQ(big::DecInt("10").digits_count(), 2);
+  ASSERT_EQ(big::DecInt(    "1").digits_count(), 1);
+  ASSERT_EQ(big::DecInt(   "-1").digits_count(), 1);
+  ASSERT_EQ(big::DecInt(   "+1").digits_count(), 1);
+  ASSERT_EQ(big::DecInt(  "001").digits_count(), 1);
+  ASSERT_EQ(big::DecInt( "-001").digits_count(), 1);
+  ASSERT_EQ(big::DecInt( "+001").digits_count(), 1);
+  ASSERT_EQ(big::DecInt(   "10").digits_count(), 2);
   ASSERT_EQ(big::DecInt("00010").digits_count(), 2);
-  ASSERT_EQ(big::DecInt("0").digits_count(), 0);
-  ASSERT_EQ(big::DecInt("0000").digits_count(), 0);
+  ASSERT_EQ(big::DecInt(    "0").digits_count(), 0);
+  ASSERT_EQ(big::DecInt( "0000").digits_count(), 0);
 
-  ASSERT_EQ((std::string)big::DecInt("0"), "0");
-  ASSERT_EQ((std::string)big::DecInt("-0"), "0");
-  ASSERT_EQ((std::string)big::DecInt("+0"), "0");
-  ASSERT_EQ((std::string)big::DecInt("0001"), "1");
-  ASSERT_EQ((std::string)big::DecInt("-0001"), "-1");
-  ASSERT_EQ((std::string)big::DecInt("+0001"), "1");
-  ASSERT_EQ((std::string)big::DecInt("0123"), "123");
+  ASSERT_EQ((std::string)big::DecInt(    "0"),    "0");
+  ASSERT_EQ((std::string)big::DecInt(   "-0"),    "0");
+  ASSERT_EQ((std::string)big::DecInt(   "+0"),    "0");
+  ASSERT_EQ((std::string)big::DecInt( "0001"),    "1");
+  ASSERT_EQ((std::string)big::DecInt("-0001"),   "-1");
+  ASSERT_EQ((std::string)big::DecInt("+0001"),    "1");
+  ASSERT_EQ((std::string)big::DecInt( "0123"),  "123");
   ASSERT_EQ((std::string)big::DecInt("-0123"), "-123");
-  ASSERT_EQ((std::string)big::DecInt("+0123"), "123");
-  ASSERT_EQ((std::string)big::DecInt("-0"), "0");
-  ASSERT_EQ((std::string)big::DecInt("+0"), "0");
+  ASSERT_EQ((std::string)big::DecInt("+0123"),  "123");
+  ASSERT_EQ((std::string)big::DecInt(   "-0"),    "0");
+  ASSERT_EQ((std::string)big::DecInt(   "+0"),    "0");
   ASSERT_EQ((std::string)big::DecInt("-0123"), "-123");
 
-  ASSERT_EQ((std::string)big::DecInt(0), "0");
-  ASSERT_EQ((std::string)big::DecInt(1), "1");
-  ASSERT_EQ((std::string)big::DecInt(-1), "-1");
-  ASSERT_EQ((std::string)big::DecInt(123), "123");
-  ASSERT_EQ((std::string)big::DecInt(-123), "-123");
+  ASSERT_EQ((std::string)big::DecInt(      0),    "0");
+  ASSERT_EQ((std::string)big::DecInt(      1),    "1");
+  ASSERT_EQ((std::string)big::DecInt(     -1),   "-1");
+  ASSERT_EQ((std::string)big::DecInt(    123),  "123");
+  ASSERT_EQ((std::string)big::DecInt(   -123), "-123");
+
+  ASSERT_FALSE(big::DecInt(      0).is_negative());
+  ASSERT_FALSE(big::DecInt(      0).is_positive());
+  ASSERT_TRUE (big::DecInt(      0).is_zero());
+
+  ASSERT_TRUE (big::DecInt(      1).is_positive());
+  ASSERT_FALSE(big::DecInt(      1).is_negative());
+  ASSERT_FALSE(big::DecInt(      1).is_zero());
+
+  ASSERT_FALSE(big::DecInt(     -1).is_positive());
+  ASSERT_TRUE (big::DecInt(     -1).is_negative());
+  ASSERT_FALSE (big::DecInt(     -1).is_zero());
+
+  ASSERT_EQ((std::string)big::DecInt::ONE, "1");
+  ASSERT_EQ((std::string)big::DecInt::ZERO, "0");
+  ASSERT_EQ((std::string)big::DecInt(), "0");
+
+  ASSERT_EQ((std::string)big::DecInt(big::DecInt(-123)), "-123");
+  ASSERT_EQ((std::string)big::DecInt(big::DecInt("-123")), "-123");
+}
+
+TEST(BigDecInt, TestComparision) {
+  big::DecInt a, b(12), o(1);
+  ASSERT_EQ((std::string)a, "0");
+  ASSERT_EQ((std::string)b, "12");
+  a = b;
+  ASSERT_EQ((std::string)a, "12");
+  ASSERT_EQ((std::string)b, "12");
+
+  ASSERT_EQ(big::DecInt(), big::DecInt::ZERO);
+  ASSERT_EQ(o, big::DecInt::ONE);
+
+  big::DecInt minus_super = std::string("-10000000000000000000000000000000000000000000001");
+  big::DecInt minus_many = std::string("-10000000000000000000000000000000000000000000000");
+  big::DecInt minus_two = -2;
+  big::DecInt minus_one = -1;
+  big::DecInt zero = 0;
+  big::DecInt plus_one = 1;
+  big::DecInt plus_two = 2;
+  big::DecInt plus_many = std::string("10000000000000000000000000000000000000000000000");
+  big::DecInt plus_super = std::string("10000000000000000000000000000000000000000000001");
+
+  ASSERT_TRUE (minus_many == minus_many);
+  ASSERT_TRUE (plus_many == plus_many);
+  ASSERT_TRUE (zero == zero);
+  ASSERT_TRUE (minus_one == minus_one);
+
+  ASSERT_TRUE (zero != plus_one);
+  ASSERT_TRUE (plus_one != minus_one);
+  ASSERT_TRUE (minus_many != plus_many);
+  ASSERT_TRUE (minus_many != zero);
+
+  ASSERT_TRUE (minus_super < minus_many);
+  ASSERT_TRUE (minus_many < minus_two);
+  ASSERT_TRUE (minus_two < minus_one);
+  ASSERT_TRUE (minus_one < zero);
+  ASSERT_TRUE (zero < plus_one);
+  ASSERT_TRUE (plus_one < plus_two);
+  ASSERT_TRUE (plus_two < plus_many);
+  ASSERT_TRUE (minus_many < plus_many);
+  ASSERT_TRUE (plus_many < plus_super);
+
+  ASSERT_TRUE (minus_many > minus_super);
+  ASSERT_TRUE (minus_two > minus_many);
+  ASSERT_TRUE (minus_one > minus_two);
+  ASSERT_TRUE (zero > minus_one);
+  ASSERT_TRUE (plus_one > zero);
+  ASSERT_TRUE (plus_two > plus_one);
+  ASSERT_TRUE (plus_many > plus_two);
+  ASSERT_TRUE (plus_many > minus_many);
+  ASSERT_TRUE (plus_super > plus_many);
+
+  ASSERT_FALSE(minus_super > minus_many);
+  ASSERT_FALSE(minus_many > minus_two);
+  ASSERT_FALSE(minus_two > minus_one);
+  ASSERT_FALSE(minus_one > zero);
+  ASSERT_FALSE(zero > plus_one);
+  ASSERT_FALSE(plus_one > plus_two);
+  ASSERT_FALSE(plus_two > plus_many);
+  ASSERT_FALSE(minus_many > plus_many);
+  ASSERT_FALSE(plus_many > plus_super);
+
+  ASSERT_FALSE(minus_many < minus_super);
+  ASSERT_FALSE(minus_two < minus_many);
+  ASSERT_FALSE(minus_one < minus_two);
+  ASSERT_FALSE(zero < minus_one);
+  ASSERT_FALSE(plus_one < zero);
+  ASSERT_FALSE(plus_two < plus_one);
+  ASSERT_FALSE(plus_many < plus_two);
+  ASSERT_FALSE(plus_many < minus_many);
+  ASSERT_FALSE(plus_super < plus_many);
+
+  ASSERT_FALSE(minus_many < minus_many);
+  ASSERT_FALSE(minus_two < minus_two);
+  ASSERT_FALSE(minus_one < minus_one);
+  ASSERT_FALSE(zero < zero);
+  ASSERT_FALSE(plus_one < plus_one);
+  ASSERT_FALSE(plus_two < plus_two);
+
+  ASSERT_FALSE (minus_two < minus_many);
+  ASSERT_FALSE (minus_one < minus_two);
+  ASSERT_FALSE (zero < minus_one);
+  ASSERT_FALSE (plus_one < zero);
+  ASSERT_FALSE (plus_two < plus_one);
+  ASSERT_FALSE (plus_many < plus_two);
+  ASSERT_FALSE (plus_many < minus_many);
+
+  ASSERT_TRUE (minus_super <= minus_many);
+  ASSERT_TRUE (minus_many <= minus_two);
+  ASSERT_TRUE (minus_two <= minus_one);
+  ASSERT_TRUE (minus_one <= zero);
+  ASSERT_TRUE (zero <= plus_one);
+  ASSERT_TRUE (plus_one <= plus_two);
+  ASSERT_TRUE (plus_two <= plus_many);
+  ASSERT_TRUE (minus_many <= plus_many);
+  ASSERT_TRUE (plus_many <= plus_super);
+
+  ASSERT_TRUE (minus_many <= minus_many);
+  ASSERT_TRUE (plus_many <= plus_many);
+  ASSERT_TRUE (zero <= zero);
+  ASSERT_TRUE (minus_one <= minus_one);
+
+  ASSERT_TRUE (minus_many >= minus_many);
+  ASSERT_TRUE (plus_many >= plus_many);
+  ASSERT_TRUE (zero >= zero);
+  ASSERT_TRUE (minus_one >= minus_one);
 }
