ede25548cb063971199d2ee145a33409b85f4038
esix <esix@live.ru>
2015-11-27 20:50:12 +0300

    Solving 600D

--- codeforces/600/D/solution.cpp
+++ codeforces/600/D/solution.cpp
@@ -9,7 +9,7 @@
 
 using namespace std;
 
-double sqr(double d) {
+long double sqr(long double d) {
   return d * d;
 }
 
@@ -19,17 +19,19 @@ int main() {
   cin.tie(0);
   ios::sync_with_stdio(false);
 
-  int x1, y1, R, x2, y2, r;
+  long long int x1, y1, R, x2, y2, r;
   cin >> x1 >> y1 >> R >> x2 >> y2 >> r;
 
   if(r > R) swap(r,R);
 
-  double d = sqrt(sqr(x1 - x2) + sqr(y1 - y2));
-  double s = sqr(r) * acos((sqr(d) + sqr(r) - sqr(R)) / (2 * d * r)) +
-             sqr(R) * acos((sqr(d) + sqr(R) - sqr(r)) / (2 * d * R)) -
-             0.5 * sqrt((-d + r + R) * (d + r - R) * (d - r + R) * (d + r + R));
+  long long int dd = (x1 - x2) * (x1 - x2)  + (y1 - y2) * (y1 - y2);
+  long double d = sqrt(dd);
 
-  cout.precision(17);
+  long double s = r * r * acos((dd + sqr(r) - sqr(R)) / (2 * d * r)) +
+                  R * R * acos((dd + sqr(R) - sqr(r)) / (2 * d * R)) -
+                  0.5 * sqrt((-d + r + R) * (d + r - R) * (d - r + R) * (d + r + R));
+
+  cout.precision(11);
   if(isnan(s)) {
     if(d < R) {
       cout << 3.141592653589793238 * r * r;
