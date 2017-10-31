b72c3b511031fd2c96b42097213633da37af722e
esix <esix@live.ru>
2017-11-01 00:07:11 +0300

    ACCEPTED mipt:009

--- mipt/009/main.scm
+++ mipt/009/main.scm
@@ -1,17 +1,18 @@
-(define cache (make-hash-table))
-
-(define (fib* n)
-  (let ((cached (hash-table-get cache n null)))
-    (if (eq? cached null)
-        (let ((val (+ (fib (- n 1)) (fib (- n 2)))))
-         (hash-table-put! cache n val)
-          val)
-        cached)))
-
-(define (fib n)
-    (if (or (= n 0) (= n 1))
-        1
-        (fib* n)))
-
+(define v1 0)
+(define v2 1)
 (define n (read))
-(display (fib n))
+
+(define (next-fib)
+  (let ((tmp v1))
+    (set! v1 v2)
+    (set! v2 (+ tmp v2))))
+
+(if (or (= n 0) (= n 1))
+    (display 1)
+    (let ((i 1))
+      (do ((i 1 (+ i 1)))
+        ((> i n))
+        (next-fib))
+      (display v2)))
+        
+
