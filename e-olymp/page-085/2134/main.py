19e95a5d9fea7c93bb80fba0989281c046f56f24
esix <esix@live.ru>
2017-11-07 12:41:58 +0300

    ACCEPTED e-olymp:2134

---
+++ e-olymp/page-085/2134/main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+x, y, x1, y1, x2, y2 = map(int, input().split(' '))
+dx, dy = x2-x1, y2-y1
+
+t = (x - x1) / dx if dx != 0 else (y - y1) / dy
+
+is_on_segment = (t >= 0 and t <= 1 and x == x1 + dx * t and y == y1 + dy * t)
+
+print ("YES" if is_on_segment else "NO")
