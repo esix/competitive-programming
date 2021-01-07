87f4ea3f4df6d229f885c98953aa9f0f20b0f9b6
esix <esix@live.ru>
2021-01-07 21:54:15 +0300

    ACCEPTED leetcode:0127

--- leetcode/0101-0150/0127/main.js
+++ leetcode/0101-0150/0127/main.js
@@ -5,23 +5,21 @@
  * @return {number}
  */
 var ladderLength = function(start, end, words) {
+    debugger;
   const N = start.length;
   const isAdjacent = (a, b) => {
       let diff = 0;
-      for (let i = 0; i < N; i++) diff += (a[i] != b[i]);
+      for (let i = 0; i < N; i++) if ((diff += (a[i] != b[i])) > 1) return false;
       return diff === 1;
   }
   if (!words.includes(end)) return 0;
   if (!words.includes(start)) words.push(start);
 
-  let visited = {}, deep = {[start]: 1};
+  let deep = {[start]: 1};
   let q = [start];
   while (q.length) {
       let word = q.shift();
-      if (visited[word]) continue;
-      visited[word] = true;
-      let neighbours = words.filter(next => !visited[next] && 
-                                            !q.includes(next) && 
+      let neighbours = words.filter(next => !deep[next] && 
                                             isAdjacent(next, word));
       neighbours.forEach(next => {
           q.push(next);
