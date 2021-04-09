5201b4e32697811758cc9d6d42c75d8a134f7aec
esix <esix@live.ru>
2021-04-10 00:22:51 +0300

    ACCEPTED leetcode:1351

--- leetcode/page-28/1351/main.js
+++ leetcode/page-28/1351/main.js
@@ -3,20 +3,21 @@
  * @return {number}
  */
  var countNegatives = function(grid) {
-  const countNegativeInVector = (vec) => {
-      let result = 0;
-      for (let i = vec.length - 1; i >= 0; i--) {
-          if (vec[i] <= 0) result++;
-          else break;
-      }
-      return result;
-  }
+    const countNegativeInVector = (vec) => {
+        let result = 0;
+        for (let i = vec.length - 1; i >= 0; i--) {
+            if (vec[i] < 0) result++;
+            else break;
+        }
+        return result;
+    }
+    
+    let result = 0;
+    for (let i = grid.length - 1; i >= 0; i--) {
+        let c = countNegativeInVector(grid[i]);
+        if (c === 0) break;
+        result += c;
+    }
+    return result;
+  };
   
-  let result = 0;
-  for (let i = grid.length - 1; i >= 0; i--) {
-      let c = countNegativeInVector(grid[i]);
-      if (c === 0) break;
-      result += c;
-  }
-  return result;
-};
