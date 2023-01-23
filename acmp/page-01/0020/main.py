0e286027276a56812c53f67f950e296469b1c459
esix <esix@live.ru>
2023-01-23 16:18:34 +0300

    ACCEPTED acmp:0020

--- acmp/page-01/0020/main.py
+++ acmp/page-01/0020/main.py
@@ -6,7 +6,12 @@ def sign(a):
     return -1 if a < 0 else 1 if a > 0 else 0
 
 N = int(inp.readline())
-ls = list(map(int, inp.readline().split(' ')[0:N]))
+ls = [int(x) for x in inp.readline().split()]
+
+if N == 1:
+    out.write('1')
+    exit(0)
+
 ss = [sign(ls[i+1] - ls[i]) for i in range(N-1)]
 
 
@@ -21,11 +26,11 @@ for i in range(1, N-1):
             max_l = l
         l = 0 if ss[i] == 0 else 1
         
-if l> max_l:
+if l > max_l:
     max_l = l
         
 answer = str(max_l + 1)
 
 
-print(answer)
+# print(answer)
 out.write(answer)
