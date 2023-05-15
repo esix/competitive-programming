be1bc19124c7a2fc07c5cd9d0bbf9d8e84726a2b
esix <esix@live.ru>
2023-05-15 11:03:36 +0300

    ACCEPTED hackerrank:mini-max-sum

---
+++ hackerrank/algorithms/mini-max-sum/main.ts
@@ -0,0 +1,44 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString: string = '';
+let inputLines: string[] = [];
+let currentLine: number = 0;
+
+process.stdin.on('data', function(inputStdin: string): void {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function(): void {
+    inputLines = inputString.split('\n');
+    inputString = '';
+
+    main();
+});
+
+function readLine(): string {
+    return inputLines[currentLine++];
+}
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function miniMaxSum(arr: number[]): void {
+    // Write your code here
+    const S = arr.reduce((acc, el) => acc + el, 0);
+    const min = Math.min(...arr);
+    const max = Math.max(...arr);
+    console.log(S - max, S - min);
+}
+
+function main() {
+
+    const arr: number[] = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    miniMaxSum(arr);
+}
