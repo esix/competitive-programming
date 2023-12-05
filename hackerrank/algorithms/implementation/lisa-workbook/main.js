f647e0ba834f1d708aa75849f80d2e9e047415be
esix <esix@live.ru>
2023-12-05 11:07:48 +0300

    ACCEPTED hackerrank:lisa-workbook

---
+++ hackerrank/algorithms/implementation/lisa-workbook/main.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'workbook' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER k
+ *  3. INTEGER_ARRAY arr
+ */
+
+function workbook(n, k, arr) {
+    let page = 1, result = 0;
+    arr.forEach((count, idx) => {
+       const chapter = idx + 1;
+       let i = 1;
+       for (; i <= count; i+= k) {
+           const minProblem = i, maxProblem = Math.min(count, i + k - 1);
+           if (minProblem <= page && page <= maxProblem) {
+               result++;
+               console.log('Has ')
+           }
+           page++;
+       }
+    });
+    return result;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const n = parseInt(firstMultipleInput[0], 10);
+
+    const k = parseInt(firstMultipleInput[1], 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const result = workbook(n, k, arr);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
