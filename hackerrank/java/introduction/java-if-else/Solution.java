b03617a4432a2b6ba9cdd07fd199c80d923ccd8d
esix <esix@live.ru>
2024-08-26 13:46:37 +0300

    ACCEPTED hackerrank:java-if-else

--- hackerrank/java/introduction/java-if-else/Solution.java
+++ hackerrank/java/introduction/java-if-else/Solution.java
@@ -8,14 +8,19 @@ import java.util.regex.*;
 
 public class Solution {
 
+
+
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         int N = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        if (N % 2 == 1 || 6 <= N && N <= 20) {
+            System.out.println("Weird");
+        } else {
+            System.out.println("Not Weird");
+        }
         scanner.close();
-        if (N % 2 == 1 || 6 <= N && N <= 20) System.out.println("Weird");
-        else System.out.println("Not Weird");
     }
 }
 
